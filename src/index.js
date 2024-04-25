3d58f27ad37d72a096adb9834f15055b873073a7
Suraj <suraj@aifly.io>
2024-04-25 08:43:08 +0530

    Day1-Commit

--- src/index.js
+++ src/index.js
@@ -1,74 +1,23 @@
 
 import Phaser from "phaser";
+import PreloadScene from "./Scenes/PreloadScene";
+import PlaScene from "./Scenes/PlayScene";
+
 const config = {
   //webGL() JS API redering 2d & 3d graphics.
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 1200,
+  height: 300,
+  pixelArt: true,
+  transparent: true,
   physics: {
     //Arcade physics - controll the gravity and vlocity.
     default: 'arcade',
     arcade:{
-      debug: true,
-      gravity:{
-        y: 0
-      }
+      debug: true 
     }
   },
-  scene: {
-    preload: preload,
-    create: create,
-    update: update
-  }
+  scene: [PreloadScene, PlaScene]
 };
-//Loading Assets: EG- Images, Animation, Music.
-function preload () {
-  //debugger
-  this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', 'assets/bird.png');
-}
-let bird = null;
-let totalDelta = 0;
-let VELOCITY = 200;
-
-//Initialise Instances
-function create () {
-  //this.add.image(x, y, imageName);
-  //Here x,y is the center of the Image, 
-  //so, basically we are moving the center of the image.
-
-  //this.add.image(0, 0, 'sky'); 
-  //this.add.image(config.width/2, config.height/2, 'sky'); 
-  //this.add.image(0, 0, 'sky').setOrigin(0,0); 
-  //this.add.image(400, 300, 'sky').setOrigin(0.5,0.5); 
-  this.add.image(0, 0, 'sky').setOrigin(0,0); 
-  bird = this.physics.add.sprite(config.width/10, config.height/2, 'bird').setOrigin(0,0);
-  bird.body.velocity.x = VELOCITY;
-  //bird.body.gravity.y = 200;
-  //console.log(bird.body);
-  //debugger
-  
-}
-//60fps - default.
-//60 * 16 ~ 1000, here 16 is the delta time
-function update(time, delta){
-  totalDelta += delta;
-  if(totalDelta >= 1000){
-    /*
-    console.log('totalDelta', totalDelta);
-    console.log(bird.body.velocity.y);
-    console.log(bird.body.gravity.y);
-    totalDelta = 0;
-    */
-   //Assignment.
-   
-  }
-  if(bird.x >= config.width - bird.width){
-    bird.body.velocity.x = -VELOCITY;
-  }else if(bird.x <= 0){
-    bird.body.velocity.x = VELOCITY;
-  }
-  //console.log(bird.body.x);
-}
 
 new Phaser.Game(config);
